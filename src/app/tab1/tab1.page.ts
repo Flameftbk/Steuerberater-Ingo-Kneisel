9eec7ee27d666816aedfad35eb737662ffd1d401
Björn Kneisel <36930665+Flameftbk@users.noreply.github.com>
2019-02-23 11:19:11 +0100

    Pointer hinzugefügt, YourXpert hinzugefügt, Toaster für YourXpert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, ToastController } from '@ionic/angular';
+import { viewAttached } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-tab1',
@@ -8,17 +9,53 @@ import { NavController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(private navCtrl: NavController) {}
+  public popUpViewed = false;
+
+  constructor(private navCtrl: NavController, private toast: ToastController) {}
+
+  ionViewWillEnter() {
+    if (this.popUpViewed === false) {
+      setTimeout(() => {
+        this.YourXpertToast();
+      }, 500);
+    } else {}
+  }
+
+  ionViewWillLeave() {
+    this.toast.dismiss();
+  }
 
   goToTeam() {
+
     this.navCtrl.navigateForward(['/team']);
   }
 
-  goToServices() {
-    this.navCtrl.navigateRoot(['/tabs/tab2']);
+  goToYourXpert() {
+    this.popUpViewed = true;
+    window.open('https://www.yourxpert.de/xpert/steuerberater/ingo.kneisel', '_blank');
   }
 
   goToPartners() {
     this.navCtrl.navigateForward(['/partners']);
+  }
+
+  async YourXpertToast() {
+    const toast = await this.toast.create({
+      message: 'Neu: Schnelle telefonische Beratung über YourXpert!',
+      showCloseButton: true,
+      position: 'top',
+      closeButtonText: 'Ansehen',
+      mode: 'ios',
+      animated: true,
+      color: 'primary',
+      duration: 8500
+    });
+    toast.present();
+
+    const dismiss = await toast.onDidDismiss();
+      if (dismiss.role === 'cancel') {
+        window.open('https://www.yourxpert.de/xpert/steuerberater/ingo.kneisel', '_blank');
+        this.popUpViewed = true;
+      }
   }
 }
